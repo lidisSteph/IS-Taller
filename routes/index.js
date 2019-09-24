d29431f58b6f611364280f232121d6d4b780285f
lidisC <lidishernandez68@gmail.com>
2019-09-23 20:53:55 -0600

    Cleaning routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 let express = require('express')
 let router = express.Router()
 
-// const Employee = require('../models').Employee
-
-
 router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Sequelize ORM TALLER' })
 })
